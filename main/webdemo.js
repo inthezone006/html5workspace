3789e29b14b6ecc6c5c18ff6864905c783a83abb
inthezone006 <rahul.menon85280@gmail.com>
2018-04-01 14:44:25 +0000

    Pushed website to a different URL and restored to a previous version of website, with tweaks in the about page

--- main/webdemo.js
+++ main/webdemo.js
@@ -1,10 +1,10 @@
-function alertbox(){
-    var alertboxtext=window.prompt("What do you want the alert to say?");
-    var myalertbox=window.open("", "Alert Box", "width=500,height=500");
-    myalertbox.document.write("<meta name=viewport content=\"width=device-width, initial-scale=1\"><style type=\"text/css\">body{font-size:28px}</style><body bgcolor='#ADD8E6'>");
-    myalertbox.document.write(alertboxtext);
-}
 function securitydemo(){
-    window.alert("The username is \'test\' and the password is \'test\' \(without the quotations\)");
-    window.open("https://html5-workspace-inthezone006.c9users.io/main/securitydemo/Success");
+   window.alert("Both the username and password is \'test\'.");
+   window.location.href="https://html5-workspace-inthezone006.c9users.io/main/securitydemo/testfile.txt";
+}
+function windowprompt(){
+    var prompttext=window.prompt("What do you want the window to say?");
+    var alertwindow=window.open("", "Alert Box", "width=500,height=500");
+    alertwindow.document.write("<meta name=viewport content=\"width=device-width, initial-scale=1\"><link rel=\"stylesheet\" type=\"text/css\" href=\"styles.css\"><link href=\"https://fonts.googleapis.com/css?family=Roboto\" rel=\"stylesheet\">");
+    alertwindow.document.write(prompttext);
 }
